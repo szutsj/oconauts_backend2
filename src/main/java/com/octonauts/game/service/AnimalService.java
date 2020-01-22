a22e3b146e9f931ae43b0d5289ba41b289cc41df
Judit Szuts <Judit.Szuts@loxon.eu>
2020-01-22 10:48:48 +0100

    patient generation scheduler

--- src/main/java/com/octonauts/game/service/AnimalService.java
+++ src/main/java/com/octonauts/game/service/AnimalService.java
@@ -129,7 +129,7 @@ public class AnimalService {
         return animalRepository.countCurePoints(user, LocalDateTime.now());
     }
 
-    @Scheduled(fixedRate = 100000)
+    @Scheduled(fixedDelay = )
     public void generatePatients() {
         if (lessPatinetThanMax()){
             Animal animal = new Animal();
